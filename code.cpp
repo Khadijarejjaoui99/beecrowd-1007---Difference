8b82d8cc54e65ff94435a45207e4d08b2e555021
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 20:56:37 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,14 @@
+// Solution of 1007 - Difference in beecrowd
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int A, B, C, D,DIFERENCA ; 
+    cin>>A >> B>>C >>D;
+    DIFERENCA  =(A*B)-(C*D);
+    cout << "DIFERENCA = " << DIFERENCA  <<"\n";
+    
+    system("pause");
+    return 0;
+}
